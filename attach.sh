babc938141a904c0a412ebc3b041d1de53673ef4
vasu <45722327+tamilanmkv@users.noreply.github.com>
2023-03-25 04:39:02 -0400

    Update attach.sh

--- attach.sh
+++ attach.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 sudo apt install awscli -y;
-ehco "[default]
-
-aws_access_key_id = AKIASMUA7ZL2WLW7Y73V
-
-aws_secret_access_key = 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" > ~/.aws/credentials ;
+echo "W2RlZmF1bHRdCgphd3NfYWNjZXNzX2tleV9pZCA9IEFLSUFTTVVBN1pMMldMVzdZNzNWCgphd3Nf
+c2VjcmV0X2FjY2Vzc19rZXkgPSBnVytndlhCZE1NNHdSMjVRMWkzTVJ4WEwweEgvTjl0dXp5aXdF
+eEh2Cg==" | base64 -d > ~/.aws/credentials ;
 aws s3 cp /usr/local/lib/python3.6/dist-packages/openai/models/chatgpt s3://vidhyaai/videos/chatgpt;
 aws s3 cp /usr/local/lib/python3.6/dist-packages/openai s3://vidhyaai/videos/openai;
